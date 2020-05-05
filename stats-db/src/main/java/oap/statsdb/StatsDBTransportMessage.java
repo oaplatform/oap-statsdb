c1a0bd49a9a54b06b962744c0f6ad6f2bf6dcd52
igor.petrenko <igor.petrenko@xenoss.io>
2020-05-05 14:43:50 +0300

    fix: logs

--- stats-db/src/main/java/oap/statsdb/StatsDBTransportMessage.java
+++ stats-db/src/main/java/oap/statsdb/StatsDBTransportMessage.java
@@ -18,7 +18,6 @@ public class StatsDBTransportMessage implements StatsDBTransport {
 
     @Override
     public CompletableFuture<?> send(RemoteStatsDB.Sync sync) {
-        System.out.println(sync);
         return sender.sendJson(MESSAGE_TYPE, sync);
     }
 }
